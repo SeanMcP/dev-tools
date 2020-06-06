1745e91d4a3909cf244f0b4f55f7928b90f9cdbc
Sean McPherson <git@seanmcp.com>
2020-06-06 14:04:12 -0400

    feat: add custom query functions

--- js/common.js
+++ js/common.js
@@ -1,1 +1,4 @@
 console.info('Made by SeanMcP – https://seanmcp.com')
+
+document.queryData = string => document.querySelector(`[data-${string}]`)
+document.queryDataAll = string => document.querySelectorAll(`[data-${string}]`)
